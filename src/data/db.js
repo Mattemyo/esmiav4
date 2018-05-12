6735655efcfda132cea392a99747958287627c0c
Mattemyo <matias97marti@gmail.com>
2018-05-12 21:19:26 +0200

    modify description and add images

--- src/data/db.js
+++ src/data/db.js
@@ -10,55 +10,82 @@ import paellaValenciana from '../images/paellaValenciana.JPG';
 import mariscoPaella from '../images/mariscoPaella.JPG';
 import idiazabal from '../images/idiazabal.jpg';
 import tortilla from '../images/tortilla.jpg';
+import serrano500 from '../images/serrano500.jpg';
+import essenbog500 from '../images/essenbog500.jpg';
+import serrano5 from '../images/serrano5.jpg';
 
 const products = [
   {
     type: 'skinka',
-    name: 'Iberico vak., 30 mån',
-    image: tjockskinka,
-    price: '550 kr / kg',
-    description:
-      'Ibericoskinka 30 månaders lufttorkad. Den svarta grisrasen avlas i naturen med ekollon och örter. Senaste månader äter den vanlig grisföda, recebo. Vikt i vacuumfrp.: 4-5kg /st.'
+    name: 'Serranoskinka skivad 500g',
+    image: serrano500,
+    price: '115 kr / st',
+    description: "Över 15 månader lufttorkad. Lätt att hantera och servera. Tips: på brödskiva med tomat och olivolja. 6 brickor per låda. Ref.706022"
   },
   {
     type: 'skinka',
-    name: 'Serranoskinka reserva 14 mån',
-    image: tjockskinka,
-    price: '175 kr / kg',
-    description:
-      'Chorizo och salchichon (salame) Iberico charkuteri. Mycket smak och doft av svart den svarta grisrasen Iberico. Serveras kallskuret på fat med paprika, oliver mm. Förpackning: 1,2kg st. 6 st/kolli = 8kg'
+    name: 'Serranoskinka 5kg vac. benfri',
+    image: serrano5,
+    price: '170 kr / kg',
+    description: "Över 15 månader lufttorkad. Utan svål och benfri för att skära direkt i skärmaskin. 2 st per låda. Ref.22120"
   },
   {
     type: 'skinka',
-    name: 'Serranoskinka 500g skivad',
+    name: 'Chorizo iberico skivad 500g',
     image: tjockskinka,
-    price: '235 kr / kg',
-    description:
-      'Serranoskinka skivad 500g. Lätt att dra varje skiva med mellanlägg. Skinkan kommer från grishona med 11 månaders lufttorkning. Serveras med med oliver eller dadlar. Förpackning: 5 st x 500g = 2,5kg/kolli.'
+    price: '115 kr / st',
+    description: 
+    'Lufttorkad 4 månader. Runt 6 centimeter i diamter. Tips: med oliver och rostad paprika. 6 brickor per låda. Ref.703422'
   },
   {
     type: 'skinka',
-    name: 'Redondo Iglesias 500g skivad',
+    name: 'Salchicon iberico skivad 500g',
+    image: tjockskinka,
+    price: '115 kr / st',
+    description: "Lufttorkad 4 månader. Lösa skivor att placera på fat. Tips: med grillad aubergine. 6 brickor per låda. Ref.703522"
+  },
+  {
+    type: 'skinka',
+    name: 'Ibericoskinka skivad 500g',
     image: redondoIglesias,
-    price: '235kr / kg',
-    description:
-      'Serranoskinka skivad 500g. Lätt att dra varje skiva med mellanlägg. Skinkan från grishona med 11 månaders lufttorkning. Linda den med dadlar, runt getjockskinka servera med oliver. Förpackning: 5 st x 500g = 2,5kg/kolli.'
+    price: '370kr / st',
+    description:"Lagrad 30 månader, utan svål. Tips: med rostade mandlar eller oliver. 6 brickor per låda. Ref.701022"
   },
   {
+    type: 'skinka',
+    name: 'Ibericoskinka vac. benfri 30 mån 5kg',
+    image: redondoIglesias,
+    price: '640 kr / kg',
+    description:"Utan svål och putsad. Skär direkt i skärmaskin. 1 st per låda. Ref.701022"
+  },
+  {
+    type: 'skinka',
+    name: 'Iberico essenbogskinka skivad 500g',
+    image: essenbog500,
+    price: '265 kr / st',
+    description:"Lagrad 2 år plattare format. Serveras med soltorkade tomater. 6 brickor per låda. Ref.702122"
+  },
+  //  chorizoes
+  {
     type: 'chorizo',
-    name: 'Chorizo och salami Iberico',
+    name: 'Chorizo fresco grill 70g',
     image: korvis,
-    price: '175 kr / kg',
-    description:
-      'Chorizo och salchichon (salame) Iberico charkuteri. Mycket smak och doft av svart den svarta grisrasen Iberico. Serveras kallskuret på fat med paprika, oliver mm. Förpackning: 1,2kg st. 6 st/kolli = 8kg'
+    price: '89 kr / kg',
+    description: "Färsk chorizo korv lagrad 1 vecka. Perfekt till grillen. Serveras genomstekt eftersom den är rå. Baserad på fläskkött, sött paprikapulver, kryddor och salt. 15 paket per låda. Varje paket innehåller 6 korvar (70g per korv)."
   },
   {
     type: 'chorizo',
-    name: 'Chorizo färsk grill',
+    name: 'Chorizo extra cular påläggschorizo 1,8kg',
     image: korv,
-    price: '89 kr / kg',
-    description:
-      'Chorizo färsk 70 gram. Paprikakorv för Bbq och stekning. Chorizo casero lagrad 350 gram. Picante smak som är mild eftersom det innehåller söt paprika & bitter paprikapulver. Perfekt till grytan eller som kallskuret. Serveras med tomatsås, lök, potatis.6,5kg per kolli.'
+    price: '128 kr / kg',
+    description: "Lufttorkad chorizo 1 månad. Hel i bit 1,8kg. Perfekt till smörgåsar. 4 st / låda."
+  },
+  {
+    type: 'chorizo',
+    name: 'Chorizo casero natural picante 450g',
+    image: korv,
+    price: '135 kr / kg',
+    description: "U-formad chorizo. Pikant inslag av bittert och sött paprikapulver. Tips: serveras direkt i tjocka skivor eller i gryta. 20 st / låda."
   },
   // KOLONIAL
   {
@@ -104,7 +131,7 @@ const products = [
   // OSTAR
   {
     type: 'ostar',
-    name: 'Manchego Altobello vak. 3,3 kg / st',
+    name: 'Manchego Altobello vac. 3,3 kg / st',
     image: altoBello,
     price: '195 kr / kg',
     amount: '2 st / låda',
