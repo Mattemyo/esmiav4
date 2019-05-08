e3875cf9e177fc7f19e6261fd6df6ebabf1cf19b
Mattemyo <matias97marti@gmail.com>
2019-05-08 22:52:35 +0200

    fix footer and about page

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -20,7 +20,8 @@ export default class AboutPage extends Component {
           <div style={{ height: 0, width: '50%', border: '2px solid #c60b1e' }} />
         </div>
         <p style={{ marginLeft: '20%', paddingTop: 30, height: '50vh' }}>
-          Esmia HB säljer spanska delikatesser.
+          Esmia HB har sedan starten 1993 sålt spanska delikatesser. Vi levererar produkter till
+          restauranger och vi hanterar även catering vid fester och evenemang för både små och stora sällskap.
         </p>
         <Footer />
       </div>
